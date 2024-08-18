c9b7267aa80c1cfe3234d94d75365a8d820161fe
Raykurnayi <raykuranyi@gmail.com>
2024-08-19 02:30:51 +0300

    Update xenoichi.py

--- xenoichi.py
+++ xenoichi.py
@@ -1,3 +1,8 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
 from flask import Flask
 from keep_alive import keep_alive
 keep_alive()
