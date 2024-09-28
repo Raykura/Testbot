8b0f2661d174380ca7c3caba2b6a99d0e0b23a52
Raykura <shoraygit@gmail.com>
2024-09-28 13:42:57 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ from importlib import import_module
 from highrise.__main__ import *
 
 # BOT SETTINGS
-bot_file_name = "xenoichi"
-bot_class_name = "xenoichi"
+bot_file_name = "MGBor"
+bot_class_name = "MGBot"
 room_id = "66bad059afeca0c24b497205"
-bot_token ="e6d943cc2e2b067461ce74bee5b90daa8aaa6960ddf75330b939a3ef2d1870f5"
+bot_token ="432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"
 
 my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
 
